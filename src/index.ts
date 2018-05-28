3a60801d459541f52484a52e0b94e3618307a29e
Rytis Alekna <ralekna@exadel.com>
2018-05-28 13:48:30 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import ChatServer from './server';
+
+new ChatServer('127.0.0.1', 5000);
---
+++ src/server/index.ts
@@ -0,0 +1,6 @@
+
+export default class ChatServer {
+    constructor(host: string, port: number) {
+
+    }
+}
