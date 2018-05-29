f41761aba8282b5608536110a30ee5cad1f377dc
Rytis Alekna <ralekna@exadel.com>
2018-05-29 12:50:20 +0300

    formating

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,49 +3,50 @@ import Express from 'express';
 import Http from 'http';
 
 export default class ChatServer {
-    constructor(private host: string, private port: number) {}
+  constructor(private host: string, private port: number) {
+  }
 
-    public init(): void {
+  public init(): void {
 
-        let app = Express();
+    let app = Express();
 
-        let server = Http.createServer(app);
+    let server = Http.createServer(app);
 
-        let io: SocketIO.Server = SocketIO(server);
+    let io: SocketIO.Server = SocketIO(server);
 
-        io.on('connection', (socket) => {
-            socket.broadcast.emit('message', { message: 'user connected', details: socket.id });
-            socket.emit('message', { message: 'welcome to chat', details: socket.id });
+    io.on('connection', (socket) => {
+      socket.broadcast.emit('message', {message: 'user connected', details: socket.id});
+      socket.emit('message', {message: 'welcome to chat', details: socket.id});
 
-            socket.on('message', (message) => {
-                socket.broadcast.emit('message', {message, details: socket.id});
-            })
+      socket.on('message', (message) => {
+        socket.broadcast.emit('message', {message, details: socket.id});
+      })
 
-            console.log('user connect', socket.id);
-        });
-        
-        server.listen(this.port, () => {
-            console.log(`listening on *:${this.port}`);
-          });
+      console.log('user connect', socket.id);
+    });
 
-        // let chat = io
-        //     .of('/chat')
-        //     .on('connection', (socket: SocketIO.Socket) => {
-        //         socket.emit('a message', {
-        //             that: 'only'
-        //             , '/chat': 'will get'
-        //         });
-        //         chat.emit('a message', {
-        //             everyone: 'in'
-        //             , '/chat': 'will get'
-        //         });
-        //     });
-        //
-        // let news = io
-        //     .of('/news')
-        //     .on('connection', (socket: SocketIO.Socket) => {
-        //         socket.emit('item', { news: 'item' });
-        //     });
-        console.log('starting server');
-    }
+    server.listen(this.port, () => {
+      console.log(`listening on *:${this.port}`);
+    });
+
+    // let chat = io
+    //     .of('/chat')
+    //     .on('connection', (socket: SocketIO.Socket) => {
+    //         socket.emit('a message', {
+    //             that: 'only'
+    //             , '/chat': 'will get'
+    //         });
+    //         chat.emit('a message', {
+    //             everyone: 'in'
+    //             , '/chat': 'will get'
+    //         });
+    //     });
+    //
+    // let news = io
+    //     .of('/news')
+    //     .on('connection', (socket: SocketIO.Socket) => {
+    //         socket.emit('item', { news: 'item' });
+    //     });
+    console.log('starting server');
+  }
 }
