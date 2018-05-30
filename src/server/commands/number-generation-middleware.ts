7277606aa5e8f11f8cd501e30bd4af85f3b2906d
Rytis Alekna <ralekna@exadel.com>
2018-05-31 01:23:43 +0300

    fixed reference error in number generator middleware

--- src/server/commands/number-generation-middleware.ts
+++ src/server/commands/number-generation-middleware.ts
@@ -1,6 +1,5 @@
 import Middleware from "./middleware";
 import Message, {MessageType} from "../message";
-import {Server, Socket} from "socket.io";
 import {setInterval} from "timers";
 
 export default class NumberGenerationMiddleware extends Middleware  {
@@ -8,9 +7,11 @@ export default class NumberGenerationMiddleware extends Middleware  {
   setRoom(room: string) {
     this.room = room;
     setInterval(() => {
-      this.namespaceWrapper.namespace
-        .to(this.room)
-        .emit(MessageType.NOTIFICATION, new Message(Math.random().toString(10), this.room))
+      if (this.namespace) {
+        this.namespace
+          .to(this.room)
+          .emit(MessageType.NOTIFICATION, new Message(Math.random().toString(10), this.room))
+      }
     }, 5000);
 
   }
